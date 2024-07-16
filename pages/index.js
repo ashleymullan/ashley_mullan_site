7de058d2fc7b9704b52bd9ae85366ec4709c0547
Ashley Mullan <54551344+ashleymullan@users.noreply.github.com>
2024-07-15 21:57:07 -0400

    update blog title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
+      <SEO title={globalData.name} description="This website has been deprecated. Please visit ashleymullan.github.io for an updated version! Thank you 😊." />
       <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
